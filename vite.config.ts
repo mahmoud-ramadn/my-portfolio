3043540664582bb49e1006a6b8535b1066259a3d
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-09-02 19:33:57 +0300

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
-import eslint from "vite-plugin-eslint"
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 })
