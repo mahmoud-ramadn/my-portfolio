cd5a8483e55e1404efc57b37a0a8420f95edc528
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-09-02 21:55:36 +0300

    feat(db): create students collection

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,10 +4,11 @@ import SplashCursor from "@/components/SplashCursor"
 
 export default function DefaultLayout() {
   return (
-    <main className="min-h-screen flex flex-col bg-black">
+    <main className="min-h-screen  overflow-hidden flex flex-col bg-black">
       <SplashCursor />
-
+ <main className=" container mx-auto px-4 ">
       <Outlet />
+ </main>
     </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import DomeGallery from "@/components/DomeGallery"
+import SplitText from "@/components/SplitText"
 import TextType from "@/components/TextType"
 
 export default function Home() {
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!")
+  }
   return (
-    <>
-      <div className=" flex items-center justify-center min-h-screen">
+    <div className=" flex flex-col items-center justify-center">
+        <div className=" my-10 flex items-center   h-[500px] flex-col justify-center"> 
+
         <TextType
           text={["Hi, I'm Mahmoud Ramadan", "Front-End Developer", "Vue, Nuxt & React"]}
           typingSpeed={75}
@@ -12,7 +18,26 @@ export default function Home() {
           showCursor={true}
           cursorCharacter="|"
         />
-      </div>
-    </>
+        <SplitText
+          text="Hello, there"
+          className="text-8xl text-white font-semibold text-center"
+          delay={100}
+          duration={0.6}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+          onLetterAnimationComplete={handleAnimationComplete}
+        />
+
+        </div>
+
+        <div  style={{ width: '100vw', height: "50vh" }} className="  p-10 overflow-hidden rounded-full">
+          <DomeGallery  segments={20} openedImageWidth="400px" openedImageHeight="400px" grayscale={false} />
+        </div>
+    </div>
   )
 }
